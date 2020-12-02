8066db6bd5f70d073a1baeab7c506da8e18584f7
suppmen <suppmen@gmail.com>
2020-12-02 09:46:00 +0100

    new git-page update after merge

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 import BeerList from "../components/BeerList";
 
 export default function Shop(props) {
