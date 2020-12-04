97166d9073e95d14dcce2301561a8daf1a62fbf2
suppmen <suppmen@gmail.com>
2020-12-04 10:59:50 +0100

    trying to remove matching on beerList.js

--- src/components/BeerList.js
+++ src/components/BeerList.js
@@ -1,20 +1,33 @@
-// import React, { useState, useEffect } from "react";
+import React from "react";
 import Beer from "./Beer";
+
 
 export default function BeerList(props) {
   const beerItemsArray = props.cartItems;
 
-  console.log(beerItemsArray, "beerItemsArray in beer list");
-
+  let counter = 0;
+  for (let i = 0; i < beerItemsArray.length; i++) {
+    let currentElement = beerItemsArray[counter];
+    for (let j = i; j < beerItemsArray.length; j++) {
+      let nextElement = beerItemsArray[j + 1];
+      if (currentElement === nextElement) {
+        beerItemsArray.splice(1, nextElement)
+      } else {
+        console.log(false, counter, currentElement, nextElement, j);
+      }
+    }
+    counter++;
+  }
   return (
     <div>
     <section className="beerCartWrapper">
       {beerItemsArray &&
         beerItemsArray.map((item, index) => {
+       
           return (
             <Beer
               key={index}
-              item={item}
+              item= {item}
               data={props.data}
               beers={props.beers}
               editCartItems={props.editCartItems}
