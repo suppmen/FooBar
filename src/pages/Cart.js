e9ac66ef4d82b43018a34d49bf8782f5b523ea37
Laufey <laufey@gmail.com>
2020-12-06 22:22:15 +0000

    cart adjustments2

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -6,6 +6,49 @@ import arrow from "../media/long-arrow.svg";
 import { Link } from "react-router-dom";
 
 export default function Cart(props) {
+  function Nobeers() {
+    if (props.notificationsCount === 0) {
+      return (
+        <div className="noBeers">
+          <h3>You have no beers on your order!</h3>
+          <Link to="/shop">
+            <button className="pay">Let's fix that</button>
+          </Link>
+        </div>
+      );
+    } else {
+      console.log("we have an order");
+      return (
+        <div className="next-step">
+          <button className="back-shop">
+            <Link to="/shop">
+              <img className="back-arrow-dark" src={arrow} alt="arrow" />
+              Continue Shopping
+            </Link>
+          </button>
+          <button className="pay">
+            <Link to="/payment">Pay</Link>
+          </button>
+        </div>
+      );
+    }
+  }
+  function Total() {
+    if (props.notificationsCount === 1) {
+      return (
+        <h3 className="cart-total">
+          A total of {props.notificationsCount} beer
+        </h3>
+      );
+    } else {
+      return (
+        <h3 className="cart-total">
+          A total of {props.notificationsCount} beers
+        </h3>
+      );
+    }
+  }
+
   return (
     <section>
       <h2>Your Order</h2>
@@ -16,12 +59,8 @@ export default function Cart(props) {
         editCartItems={props.editCartItems}
         notificationsCount={props.notificationsCount}
       />
-      <p>Her you will see your order</p>
-      <div>
-        <button>
-          <Link to="/payment">Pay</Link>
-        </button>
-      </div>
+      <Nobeers />
+      <div></div>
     </section>
   );
 }
