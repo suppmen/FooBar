e59cb00604067fa995351cf0640f762c64a12cc0
Laufey <laufey@gmail.com>
2020-12-10 23:52:14 +0000

    both home page updated and form page fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,19 @@ import React from "react";
 // import Shop from "./Shop";
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import logo from "../media/logo_white.svg";
+import logo from "../media/beer-mug.svg";
+// import circle from "../media/circle.svg";
 
 export default function Home(props) {
   return (
     <div className="Home">
+      {/* <img className="circle" src={circle} alt="circle" /> */}
       <article className="home-wrapper">
+        <img className="logo" src={logo} alt="logo" />
         <div className="home-title">
-          <h1>Welcome to</h1>
+          {/* <h1>Welcome to</h1> */}
           <h1>FooBar</h1>
         </div>
-        <img className="logo" src={logo} alt="logo" />
 
         {/* <button className="home-btn">Start</button> */}
       </article>
