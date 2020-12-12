292ff0646faa21bf83ad0216b4b37f47e61b7640
Ariadna <aria0401@kea.stud.dk>
2020-12-12 20:35:27 +0100

    form with new design

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Form from "../components/Form";
-import Header from "../components/Header";
+import cart from "../media/cart.svg";
+
 
 export default function Payment(props) {
 
   return (
     <section>
-      <Header notificationsCount={props.notificationsCount}/>
+      <div className="top">
+        <div className="total-price">
+        <img className="payment-cart" src={cart} alt="cart" />
+        <h2>= 45 DKK</h2>
+        </div>
+      <div className="mask">
+        <div className="triangle"></div>
+      </div>
+      </div>
+
       <Form sendPostRequest={props.sendPostRequest} cartItems={props.cartItems}  />
+
     </section>
   );
 }
